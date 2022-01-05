3a5bceb567dbce148ad8439bf7a9d0bff6b042e6
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-01-05 07:55:34 -0300

    Creating and implementing Calculator class

---
+++ src/main/java/br/com/rodrigodonizettio/calc/Calculator.java
@@ -0,0 +1,26 @@
+package br.com.rodrigodonizettio.calc;
+
+public class Calculator {
+
+    public Calculator() { }
+
+    public int sumWillReturn2() {
+        return 1 + 1;
+    }
+
+    public int sum(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int sub(int number1, int number2) {
+        return number1 - number2;
+    }
+
+    public int mul(int number1, int number2) {
+        return number1 * number2;
+    }
+
+    public int div(int dividend, int divisor) {
+        return dividend / divisor;
+    }
+}
